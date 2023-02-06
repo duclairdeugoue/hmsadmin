69a94b7ba6e7b5d8b1546c14fafc5bfc2cf90d0a
duclairdeugoue <87701146+duclairdeugoue@users.noreply.github.com>
2023-02-06 15:34:47 +0100

    update: auth/login form

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -12,10 +12,10 @@
 <p class="account-subtitle">Access to our dashboard</p>
 <form action="<?= base_url() ?>/dashboard/dashboard">
   <div class="form-group">
-    <input class="form-control" type="text" placeholder="Email">
+    <input class="form-control" type="email" placeholder="Email">
   </div>
   <div class="form-group">
-    <input class="form-control" type="text" placeholder="Password">
+    <input class="form-control" type="password" placeholder="Password">
   </div>
   <div class="form-group">
     <button class="btn btn-primary btn-block" type="submit">Login</button>
@@ -23,7 +23,11 @@
 </form>
 <div class="text-center forgotpass"><a href="<?= base_url() ?>/auth/forget_password">Forgot Password?</a> </div>
 <div class="login-or"> <span class="or-line"></span> <span class="span-or">or</span> </div>
-<div class="social-login"> <span>Login with</span> <a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a><a href="#" class="google"><i class="fab fa-google"></i></a> </div>
+<div class="social-login">
+  <span>Login with</span>
+  <a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a>
+  <a href="#" class="google"><i class="fab fa-google"></i></a>
+</div>
 <div class="text-center dont-have">Don’t have an account? <a href="<?= base_url() ?>/auth/register">Register</a></div>
 <?= $this->endSection() ?>
 
--- app/Views/layouts/auth.php
+++ app/Views/layouts/auth.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-  <title>Hotel Dashboard Template</title>
+  <title>Hotel Authentication</title>
   <link rel="shortcut icon" type="image/x-icon" href="<?= base_url() ?>/assets/img/favicon.png">
 
   <!-- Header assets (external libraries) -->
